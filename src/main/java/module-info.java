f6b68bb0acf4965e7325ce4341246676ab41aa1d
LucasRP <rodrigues.lrp21@gmail.com>
2025-02-07 10:54:46 -0300

    Inicio de tudo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module projeto.projeto_poo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens projeto.projeto_poo to javafx.fxml;
+    exports projeto.projeto_poo;
+}
