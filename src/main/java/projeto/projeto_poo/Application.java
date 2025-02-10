ec9f28a82b16d334e165ea1347454f83c3b8f237
Lucena <joaopedrolucena568@gmail.com>
2025-02-10 00:45:39 -0300

    estilização da application-view, adição do ícone e nome e idealização de nova estrutura usando Vbox

--- src/main/java/projeto/projeto_poo/Application.java
+++ src/main/java/projeto/projeto_poo/Application.java
@@ -2,6 +2,7 @@ package projeto.projeto_poo;
 
 import javafx.stage.Stage;
 import projeto.projeto_poo.controller.ScreenManager;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
 
@@ -10,6 +11,14 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         ScreenManager.setStage(stage);
         ScreenManager.mudarScene("/projeto/projeto_poo/view/application-view.fxml", 600, 800);
+
+        stage.setMinWidth(400);
+        stage.setMinHeight(500);
+
+        stage.setTitle("Debug & Win");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/projeto/projeto_poo/view/images/icon.jpg")));
+
+        stage.show();
     }
 
     public static void main(String[] args) {
